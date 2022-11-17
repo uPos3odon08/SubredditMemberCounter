ea115e6aad2d82c73f730a38f2373230d72887d7
uPos3odon08 <82838374+uPos3odon08@users.noreply.github.com>
2022-11-17 20:22:59 +0100

    Update Main.py
    fixed a bug where it wouldn't update the member counter

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ reddit = praw.Reddit(
     client_secret="YOUR CLIENT SECRET HERE",
     user_agent="subreddit member amount scraper 1.5 by u/[YOUR REDDIT USERNAME HERE]",
 )
-subs = reddit.subreddit("[NAME OF THE SUBREDDIT WITHOUT THE r/]").subscribers
+
 subreddit = reddit.subreddit("[NAME OF THE SUBREDDIT WITHOUT THE r/]")
 
 
@@ -17,6 +17,7 @@ print("Author = u/Pos3odon08")
 x = 1
 while True: 
  now = datetime.now()
+ subs = reddit.subreddit("[NAME OF THE SUBREDDIT WITHOUT THE r/]").subscribers
  current_time = now.strftime("%Hh %Mm %Ss")
  print(subreddit.display_name, end= '')
  print("     subs", subs, end= '') 
