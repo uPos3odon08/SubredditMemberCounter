b34d5b598bddc6fe2c58c4af271376043113c82d
uPos3odon08 <82838374+uPos3odon08@users.noreply.github.com>
2022-11-17 13:55:11 +0100

    Update Main.py
    added features to see the time of the print

--- Main.py
+++ Main.py
@@ -1,15 +1,21 @@
 import praw
 import time
+from datetime import datetime
 
 reddit = praw.Reddit(
-    client_id="[CLIENT ID HERE]",
-    client_secret="[CLIENT SECRET HERE]",
-    user_agent="[USER AGENT HERE]",
+    client_id="YOUR CLIENT ID HERE",
+    client_secret="YOUR CLIENT SECRET HERE",
+    user_agent="subreddit member amount scraper 1.5 by u/[YOUR REDDIT USERNAME HERE]",
 )
-subs = reddit.subreddit("[ENTER SUBREDDIT HERE WITHOUT THE r/").subscribers
+subs = reddit.subreddit("NAME OF THE SUBREDDIT WITHOUT THE r/").subscribers
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
 
 x = 1
 while True: 
- print(subs)
- time.sleep(100)
+ print("subs", subs, end= '') 
+ print("     🕒 =", current_time)
+ time.sleep(60)
 x += 1
